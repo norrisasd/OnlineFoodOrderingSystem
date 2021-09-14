d07752012de57594764123dffa08495e9a41ad9a
NORRIS HIPOLITO <NORRISHIPOLITO@example.com>
2021-09-14 12:33:47 +0800

    models and init

--- OnlineFoodOrderingSystem/Bossing/models.py
+++ OnlineFoodOrderingSystem/Bossing/models.py
@@ -3,23 +3,24 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    user_id=models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=11)
     is_admin = models.BooleanField()
 
     class meta:
-        db_table = 'tblUser'
+        db_table = 'user'
 
 class Product(models.Model):
-    product_id = models.CharField(max_length=10)
-    product_name = models.CharField(max_length=20)
-    product_category = models.CharField(max_length=10)
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    product_category = models.CharField(max_length=50)
     price = models.FloatField()
 
     class meta:
-        db_table = 'tblProduct'
+        db_table = 'product'
 
 
--- OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/settings.py
+++ OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +81,15 @@ WSGI_APPLICATION = 'OnlineFoodOrderingSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbyamifood',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
