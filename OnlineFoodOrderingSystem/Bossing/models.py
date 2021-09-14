169ebad50b5b32524ec80eb25f2b50e60bd75fdf
jerichonaces <nacesjericho@gmail.com>
2021-09-14 10:12:06 +0800

    models for tables user and product

--- OnlineFoodOrderingSystem/Bossing/models.py
+++ OnlineFoodOrderingSystem/Bossing/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=11)
+    is_admin = models.BooleanField()
+
+    class meta:
+        db_table = 'tblUser'
+
+class Product(models.Model):
+    product_id = models.CharField(max_length=10)
+    product_name = models.CharField(max_length=20)
+    product_category = models.CharField(max_length=10)
+    price = models.FloatField()
+
+    class meta:
+        db_table = 'tblProduct'
+
+
