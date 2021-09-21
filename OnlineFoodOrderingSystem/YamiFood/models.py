95815b5d0d274a5bfc81583b5af5c2210d2b85a7
jerichonaces <nacesjericho@gmail.com>
2021-09-21 21:44:58 +0800

    Added tables; text in footer

--- OnlineFoodOrderingSystem/YamiFood/models.py
+++ OnlineFoodOrderingSystem/YamiFood/models.py
@@ -23,4 +23,39 @@ class Product(models.Model):
     class meta:
         db_table = 'product'
 
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_ordered = models.DateField(auto_now_add=True)
+    total_price = models.FloatField()
 
+    class meta:
+        db_table = 'order'
+
+class Order_Details(models.Model):
+    order_details_id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class meta:
+        db_table = 'order_details'
+
+class Delivery(models.Model):
+    delivery_id = models.AutoField(primary_key=True)
+    delivery_carrier = models.CharField(max_length=50)
+
+    class meta:
+        db_table = 'delivery'
+
+class Receiver(models.Model):
+    receiver_id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    delivery_id = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    receiver_name = models.CharField(max_length=100)
+    receiver_contact = models.CharField(max_length=11)
+    receiver_address = models.CharField(max_length=100)
+    date_received = models.DateField()
+    time_received = models.TimeField()
+    class meta:
+        db_table = 'receiver'
--- OnlineFoodOrderingSystem/YamiFood/static/Admin/js/Dashboard.js
+++ OnlineFoodOrderingSystem/YamiFood/static/Admin/js/Dashboard.js
@@ -58,6 +58,66 @@ var dt1 = $('#dataTable1').DataTable({
     "responsive": true,
 });
 
+var dt2 = $('#dataTable2').DataTable({
+    "oLanguage": {
+        "sLengthMenu": "Show Entries _MENU_",
+    },
+    dom: "<'row d-flex flex-row align-items-end'>tr<'row d-flex flex-row align-items-end'<'col-auto'l><'col-md-8'i><'col-md-2'p>>",
+    "lengthMenu": [5,10, 25, 50,],
+    "pageLength": 5,
+    "order": [],
+    "columnDefs": [{
+        "targets": 0,
+        "orderable": false,
+        "className": "select-checkbox",
+    },{
+        "targets": 5,
+        "orderable": false,
+        "className": "text-center",
+        width: 100,
+    }],
+    select: {
+        style: 'multi',
+        selector: 'tr>td:nth-child(1)'
+    },
+    "paging": true,
+    "searching": true,
+    "ordering": true,
+    "info": true,
+    "autoWidth": false,
+    "responsive": true,
+});
+
+var dt3 = $('#dataTable3').DataTable({
+    "oLanguage": {
+        "sLengthMenu": "Show Entries _MENU_",
+    },
+    dom: "<'row d-flex flex-row align-items-end'>tr<'row d-flex flex-row align-items-end'<'col-auto'l><'col-md-8'i><'col-md-2'p>>",
+    "lengthMenu": [5,10, 25, 50,],
+    "pageLength": 5,
+    "order": [],
+    "columnDefs": [{
+        "targets": 0,
+        "orderable": false,
+        "className": "select-checkbox",
+    },{
+        "targets": 3,
+        "orderable": false,
+        "className": "text-center",
+        width: 100,
+    }],
+    select: {
+        style: 'multi',
+        selector: 'tr>td:nth-child(1)'
+    },
+    "paging": true,
+    "searching": true,
+    "ordering": true,
+    "info": true,
+    "autoWidth": false,
+    "responsive": true,
+});
+
 // new $.fn.dataTable.Buttons(dt, {
 //     "buttons": [{
 //         extend: 'excel',
@@ -88,4 +148,6 @@ var dt1 = $('#dataTable1').DataTable({
 function hideAll(){
     w3.hide('#UserTable');
     w3.hide('#ProductTable');
+    w3.hide('#OrderTable');
+    w3.hide('#DeliveryTable');
 }
