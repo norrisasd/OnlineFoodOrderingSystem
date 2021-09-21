72aea80e15684dac3c18b21e826c21f37cd0a271
NORRIS HIPOLITO <NORRISHIPOLITO@example.com>
2021-09-21 22:09:57 +0800

    changes

--- OnlineFoodOrderingSystem/YamiFood/views.py
+++ OnlineFoodOrderingSystem/YamiFood/views.py
@@ -1,30 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
+from django.views.generic.base import RedirectView
 from .forms import *
 
 # Create your views here.
 class IndexView(View):
     def get(self,request):
         return render(request,'./pages/index.html',{'nbar':'home'})
+
 #About View
 class AboutView(View):
     def get(self,request):
         return render(request,'./pages/about.html',{'nbar':'about'})
+
 class ContactView(View):
     def get(self,request):
         return render(request,'./pages/contact.html',{'nbar':'contact'})
+
 class MenuView(View):
     def get(self,request):
         return render(request,'./pages/menu.html',{'nbar':'menu'})
+
 class FeaturesView(View):
     def get(self,request):
         return render(request,'./pages/features.html',{'nbar':'features'})
+
 class LoginView(View):
     def get(self,request):
         return render(request,'./pages/login.html',{'nbar':'login'})
+
 class SignupView(View):
     def get(self,request):
         return render(request,'./pages/signup.html',{'nbar':'signup'})
+
+    def post(self, request):
+        form= UserForm(request.POST)
+        if form.is_valid():
+            print("keke",request.POST)
+        return redirect('./login')
+
 class DashboardView(View):
     def get(self,request):
         users = User.objects.all()
