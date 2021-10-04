9e4c7110ccc85adfbcd2018d5fd6ff8a3442db5c
jerichonaces <nacesjericho@gmail.com>
2021-10-04 09:33:10 +0800

    asd

--- OnlineFoodOrderingSystem/YamiFood/views.py
+++ OnlineFoodOrderingSystem/YamiFood/views.py
@@ -104,6 +104,14 @@ class DashboardView(View):
                 fs = FileSystemStorage()
                 fs.save(file.name,file)
                 Pform.save()
-                return JsonResponse({'status':True},status=200)
+            elif request.POST.get("username") != "":
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                first_name = request.POST.get('first_name')
+                last_name = request.POST.get('last_name')
+                phone_number = request.POST.get('phone_number')
+                Uform = User(username = username, password = password, first_name = first_name, last_name = last_name, phone_number = phone_number, is_admin = True)
+                Uform.save()
+            return JsonResponse({'status':True},status=200)
         else:
             return JsonResponse({'status':False},status=200)
