4753e1cad1c7b0f48843149bfac4a2ad57ce102a
jerichonaces <nacesjericho@gmail.com>
2021-10-14 10:09:58 +0800

    deleted email

--- OnlineFoodOrderingSystem/YamiFood/views.py
+++ OnlineFoodOrderingSystem/YamiFood/views.py
@@ -130,7 +130,7 @@ class DashboardView(View):
             first_name = request.POST.get('first_name')
             last_name = request.POST.get('last_name')
             phone_number = request.POST.get('phone_number')
-            update_user = User.objects.filter(user_id=user_id).update(username = username, password = password, first_name = first_name, last_name = last_name, phone_number = phone_number)
+            User.objects.filter(user_id=user_id).update(username = username, password = password, first_name = first_name, last_name = last_name, phone_number = phone_number)
             status = True
 
         if request.POST.get("request") == "updateProduct":
